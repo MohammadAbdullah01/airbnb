24750e3d4dce6d72c4cef6ae8ba3b5dbf5f18148
Mohammad-Abdullah <abdullahofficial2022@gmail.com>
2022-08-02 23:32:29 +0600

    date picker added

--- components/header.jsx
+++ components/header.jsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Image from 'next/image'
 import airbnb from '../assets/logo.png'
 import { SearchIcon, MenuIcon, UserCircleIcon, GlobeAltIcon } from '@heroicons/react/solid'
+import 'react-date-range/dist/styles.css'; // main style file
+import 'react-date-range/dist/theme/default.css'; // theme css file
+import { DateRangePicker } from 'react-date-range';
 
 const Header = () => {
+    const [inputValue, setInputValue] = useState('')
+    const [startDate, setStartDate] = useState(new Date())
+    const [endDate, setEndDate] = useState(new Date())
+    const handleSelect = (ranges) => {
+        {
+            selection: {
+                setStartDate(ranges.selection.startDate);
+                setEndDate(ranges.selection.endDate)
+            }
+        }
+    }
+    const selectionRange = {
+        startDate: startDate,
+        endDate: endDate,
+        key: 'selection',
+    }
     return (
         <header className='grid grid-cols-3 shadow-md bg-white sticky top-0 p-3 md:p-5 md:px-10 z-50'>
             {/* top  */}
@@ -12,7 +31,7 @@ const Header = () => {
             </div>
             {/* middle */}
             <div className='flex h-10 items-center justify-between md:border-2 md:rounded-full md:shadow-sm mx-2'>
-                <input className='bg-transparent focus:outline-none w-full px-3 text-sm' type="text" placeholder='start searching here' />
+                <input value={inputValue} onChange={(e) => setInputValue(e.target.value)} className='bg-transparent focus:outline-none w-full px-3 text-sm' type="text" placeholder='start searching here' />
                 <SearchIcon className="h-8 bg-red-400 rounded-full p-2 text-white hidden md:inline cursor-pointer md:mr-1" />
             </div>
             {/* right  */}
@@ -24,6 +43,16 @@ const Header = () => {
                     <UserCircleIcon className='h-6' />
                 </div>
             </div>
+            {inputValue && <>
+                <div className='flex col-span-3 mx-auto'>
+                    <DateRangePicker
+                        ranges={[selectionRange]}
+                        onChange={handleSelect}
+                        minDate={new Date()}
+                        rangeColors={['#F87171']}
+                    />
+                </div>
+            </>}
         </header>
     );
 };
